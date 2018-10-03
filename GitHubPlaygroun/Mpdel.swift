c7bf227126a5b31d893eeee34cbd218ff8108ac4
Jaugeni <jaugeni@gmail.com>
2018-10-03 13:24:50 -0400

    5

---
+++ GitHubPlaygroun/Mpdel.swift
@@ -0,0 +1,14 @@
+//
+//  Mpdel.swift
+//  GitHubPlaygroun
+//
+//  Created by YAUHENI IVANIUK on 10/3/18.
+//  Copyright © 2018 Yauheni Ivaniuk. All rights reserved.
+//
+
+import Foundation
+
+struct Model {
+  var mode: String
+  var next: Int
+}

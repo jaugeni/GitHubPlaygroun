eefcb26b3aa5a2bab47162dcffdee2b79ad81139
Jaugeni <jaugeni@gmail.com>
2018-10-03 13:07:24 -0400

    Initial Commit

---
+++ GitHubPlaygroun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubPlaygroun
+//
+//  Created by YAUHENI IVANIUK on 10/3/18.
+//  Copyright © 2018 Yauheni Ivaniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
